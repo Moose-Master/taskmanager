e3550f46290729a57ce07252ba33159df5b50395
Moose-Master <korivan888@gmail.com>
2023-08-25 16:29:23 -0500

    E!

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world! - SupaMaggie70");
+        System.out.println("Hello, All of existence - Moose_Master");
     }
 }
