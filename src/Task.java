bdf4d142d0abdd16a1f07832291e26fadcf40893
SupaMaggie70Incorporated <magnus.larsson.mn@gmail.com>
2023-08-25 17:19:42 -0500

    Added basic structure

--- src/Task.java
+++ src/Task.java
@@ -2,5 +2,24 @@
 The task class, individual tasks will be represented by this
  */
 
+import java.util.Date;
+
 public class Task {
+    public String Name;
+    public String Description;
+    public Date DueDate;
+    public Urgency Urgency;
+
+    public Task(String name, String description, Date date, Urgency urgency) {
+        Name = name;
+        Description = description;
+        DueDate = date;
+        this.Urgency = urgency;
+    }
+    public enum Urgency {
+        None,
+        Mild,
+        Moderate,
+        Critical
+    }
 }
--- src/TaskList.java
+++ src/TaskList.java
@@ -1,5 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+
 /*
 The list of tasks and extra information, such as the task set's name and user
  */
 public class TaskList {
+    public String Name;
+    public List<Task> Tasks;
+
+    public TaskList() {
+        Name = "Default";
+        Tasks = new ArrayList<>();
+    }
 }
