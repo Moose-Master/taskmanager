71b9a7695649d0b0fee5a995d44a39e4b2276e51
Moose-Master <korivan888@gmail.com>
2023-08-25 17:55:50 -0500

    Started a function choes if you want to read or write to you task list

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
+import java.util.Scanner;
+
 public class Main {
+    static Scanner input = new Scanner(System.in);
     public static void main(String[] args) {
         System.out.println("Hello world! - SupaMaggie70");
         System.out.println("Hello, All of existence - Moose_Master");
+        System.out.println("What would you like to do? ");
+        TerminalInterace.main_loop();
+
+
+        }
     }
-}
--- src/TerminalInterace.java
+++ src/TerminalInterace.java
@@ -3,4 +3,20 @@ Allows us to test this program before making the GUI
  */
 
 public class TerminalInterace {
+
+    public static void main_loop() {
+        while (true) {
+            String input = Main.input.nextLine();
+            if (input.equalsIgnoreCase("read")) {
+                System.out.println("List of tasks goes here");
+            } else if (input.equalsIgnoreCase("write") || input.equalsIgnoreCase("add")) {
+                System.out.println("A way to add stuff goes here");
+            } else if (input.equalsIgnoreCase("stop") || input.equalsIgnoreCase("end")) {
+                break;
+            } else {
+                System.out.println("Cmd not recognized, Please try again");
+            }
+
+        }
+    }
 }
