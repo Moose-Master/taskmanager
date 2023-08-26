23593de235d20eaf603de6db1e0ec1bc32d76076
Moose-Master <korivan888@gmail.com>
2023-08-25 20:37:34 -0500

    MADE IT WORK!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world! - SupaMaggie70");
         System.out.println("Hello, All of existence - Moose_Master");
-        System.out.println("What would you like to do? ");
+
         TerminalInterace.main_loop();
 
 
--- src/TerminalInterace.java
+++ src/TerminalInterace.java
@@ -2,21 +2,49 @@
 Allows us to test this program before making the GUI
  */
 
-public class TerminalInterace {
+import java.util.Date;
 
+public class TerminalInterace {
+    static TaskList t_list = SaveManager.Load();
     public static void main_loop() {
         while (true) {
+            System.out.println("What would you like to do? ");
             String input = Main.input.nextLine();
             if (input.equalsIgnoreCase("read")) {
-                SaveManager.Save(Main.input.nextLine());
+                for(Task t : t_list.Tasks) {
+                    System.out.println("\n" +"Name " + t.Name + "\n" + "Description " + t.Description + "\n" + "Urgency " + t.Urgency + "\n" + "DueDate " + t.DueDate);
+                }
             } else if (input.equalsIgnoreCase("write") || input.equalsIgnoreCase("add")) {
-                SaveManager.Load();
-            } else if (input.equalsIgnoreCase("stop") || input.equalsIgnoreCase("end")) {
-                break;
+                save_text();
+            } else if (input.equalsIgnoreCase("stop") || input.equalsIgnoreCase("end") || input.equalsIgnoreCase("exit") || input.equalsIgnoreCase("quit")) {
+                System.exit(0);
             } else {
                 System.out.println("Cmd not recognized, Please try again");
             }
 
         }
     }
+
+    public static void save_text(){
+        System.out.println("Name? ");
+        String t_name = Main.input.nextLine();
+        System.out.println(("Desrption? "));
+        String t_descrpt = Main.input.nextLine();
+        System.out.println("On a scale from 0-3 how uragent is it?");
+        String t_urgancy_1 = Main.input.nextLine();
+        String t_urgancy;
+        if (t_urgancy_1.equalsIgnoreCase("0"))
+             t_urgancy = "None";
+        else if (t_urgancy_1.equalsIgnoreCase("1"))
+             t_urgancy = "Mild";
+        else if (t_urgancy_1.equalsIgnoreCase("2"))
+             t_urgancy = "Moderate";
+        else if (t_urgancy_1.equalsIgnoreCase("3"))
+            t_urgancy = "Critical";
+        else
+            t_urgancy = "None";
+        t_list.Add(new Task(t_name,t_descrpt,new Date(),Urgency.valueOf(t_urgancy)));
+    }
+
 }
+
